7466e3cd36a0e9aa5e78e6fe463bd07e01633bac
Brian Reaume <bkreaume@gmail.com>
2017-03-01 19:01:36 -0500

    Chapter 2 excercises

---
+++ calc.rb
@@ -0,0 +1,7 @@
+hours_in_year = 365*24
+seconds_year = (hours_in_year*60)
+age=49
+puts "#{hours_in_year} Hours in a year" 
+puts "#{(hours_in_year*60)/10} Minutes In decade"
+puts  "I am  #{(seconds_year*60)*age} seconds old!!!"
+puts "Chris is #{(1160000000/seconds_year)/60} Years old!!!"
