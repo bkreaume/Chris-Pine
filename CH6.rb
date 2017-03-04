e562ff7188f5370e529b3304fbf7c60be424f570
Brian Reaume <bkreaume@gmail.com>
2017-03-04 09:37:00 -0500

    Chapter 6

---
+++ CH6.rb
@@ -0,0 +1,13 @@
+#Chapter 6
+
+# #6.2 - Angry Boss
+# puts 'Boss: What do you want?'
+# text = gets.chomp
+# puts 'Boss: Did you say you want "' + text.upcase + "?!?" + '...Get Lost!!!'
+
+#Table of Contents
+# line_width = 50
+puts 'Table of Contents'.center(50)
+puts 'Chapter 1:'.ljust(12) + 'Getting Started'.ljust(23)+ 'page 1'.ljust(15)
+puts 'Chapter 2:'.ljust(12) + 'Numbers'.ljust(23) + 'page 9'.ljust(15)
+puts 'Chapter 3:'.ljust(12) + 'Letters'.ljust(23)+ 'page 13'.ljust(15)
